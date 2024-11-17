8b3432bb3dec956b568ea678d59b932985c1f182
Louis Friedrich <48963330+louisfrdrchcoding@users.noreply.github.com>
2024-11-17 23:14:35 +0100

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,1 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from binance.spot import Spot
+from datetime import datetime
+import re
 
+client = Spot()
+data = client.klines("BTCUSDT", "1h", limit=1)
+date = client.time()
+
+print(date)
+
+seconds1 = re.search(r"\+", date).group()
+
+#for i in data:
+ #   print(i)
+
+
+milliseconds = seconds1
+
+    # Convert milliseconds to seconds
+seconds = milliseconds / 1000
+    # Convert to a datetime object
+normal_time = datetime.utcfromtimestamp(seconds)
+    # Format the datetime object to a readable string
+formatted_time = normal_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]  # Exclude last 3 digits of microseconds
+
+# Example usage
+print("Normal Time:", formatted_time)
+
+#fig, ax = plt.subplots()
+#ax.plot(x, y)
+#plt.show()
